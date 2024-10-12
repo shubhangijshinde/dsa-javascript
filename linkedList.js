2ce2f2a3af9103b6f5b3557d37cc0244285ffce3
shubhangijshinde <shubhangishinde1089@gmail.com>
2024-10-11 21:57:22 -0700

    Added linked list class, methods

--- linkedList.js
+++ linkedList.js
@@ -1,63 +1,143 @@
-/**
- * Linked List Common patterns
- * 
- * 1. Two Pointers (Fast and Slow)
- * 2. Reverse Linked List
- * 3. Merged Lists
- * 4. Cycle detection removal
- * 5. Removing N-th node from end
- * 6. Finding intersection of two linked lists
- * 7. Palindrome Check
- * 8. Flattening Multilevel Doubly Linked List
- */
+/* LinkedList */
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} list1
- * @param {ListNode} list2
- * @return {ListNode}
- */
-var mergeTwoLists = function(list1, list2) {
-    let root = new ListNode();
-    let cur = root;
-
-    while(list1 && list2) {
-        if(list1.val > list2.val) {
-            cur.next = list2;
-            list2 = list2.next;
-        }
-        else {
-            cur.next = list1;
-            list1 = list1.next;
-        }
-        cur = cur.next;
+function LinkedList() { 
+    var length = 0; 
+    var head = null; 
+  
+    var Node = function(element){
+      this.element = element; 
+      this.next = null; 
+    }; 
+  
+    this.size = function(){
+      return length;
+    };
+  
+    this.head = function(){
+      return head;
+    };
+  
+    this.add = function(element){
+      var node = new Node(element);
+      if(head === null){
+          head = node;
+      } else {
+          var currentNode = head;
+  
+          while(currentNode.next){
+              currentNode  = currentNode.next;
+          }
+  
+          currentNode.next = node;
+      }
+  
+      length++;
+    }; 
+  
+    this.remove = function(element){
+      var currentNode = head;
+      var previousNode;
+      if(currentNode.element === element){
+          head = currentNode.next;
+      } else {
+          while(currentNode.element !== element) {
+              previousNode = currentNode;
+              currentNode = currentNode.next;
+          }
+  
+          previousNode.next = currentNode.next;
+      }
+  
+      length --;
+    };
+    
+    this.isEmpty = function() {
+      return length === 0;
+    };
+  
+    this.indexOf = function(element) {
+      var currentNode = head;
+      var index = -1;
+  
+      while(currentNode){
+          index++;
+          if(currentNode.element === element){
+              return index;
+          }
+          currentNode = currentNode.next;
+      }
+  
+      return -1;
+    };
+  
+    this.elementAt = function(index) {
+      var currentNode = head;
+      var count = 0;
+      while (count < index){
+          count ++;
+          currentNode = currentNode.next
+      }
+      return currentNode.element;
+    };
+    
+    
+    this.addAt = function(index, element){
+      var node = new Node(element);
+  
+      var currentNode = head;
+      var previousNode;
+      var currentIndex = 0;
+  
+      if(index > length){
+          return false;
+      }
+  
+      if(index === 0){
+          node.next = currentNode;
+          head = node;
+      } else {
+          while(currentIndex < index){
+              currentIndex++;
+              previousNode = currentNode;
+              currentNode = currentNode.next;
+          }
+          node.next = currentNode;
+          previousNode.next = node;
+      }
+      length++;
     }
-
-    cur.next = list1 || list2;
-    return root.next;
-};
-
-
-/**
- * 141. Linked List Cycle
- * @param {ListNode} head
- * @return {boolean}
- */
-var hasCycle = function(head) {
-    let slow = head;
-    let fast = head;
-
-    while(fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-        if(slow === fast) return true;
+    
+    this.removeAt = function(index) {
+      var currentNode = head;
+      var previousNode;
+      var currentIndex = 0;
+      if (index < 0 || index >= length){
+          return null
+      }
+      if(index === 0){
+          head = currentNode.next;
+      } else {
+          while(currentIndex < index) {
+              currentIndex ++;
+              previousNode = currentNode;
+              currentNode = currentNode.next;
+          }
+          previousNode.next = currentNode.next
+      }
+      length--;
+      return currentNode.element;
     }
-
-    return false;
-};
+  
+  } 
+  
+  var conga = new LinkedList();
+  conga.add('Kitten');
+  conga.add('Puppy');
+  conga.add('Dog');
+  conga.add('Cat');
+  conga.add('Fish');
+  console.log(conga.size());
+  console.log(conga.removeAt(3));
+  console.log(conga.elementAt(3));
+  console.log(conga.indexOf('Puppy'));
+  console.log(conga.size());
